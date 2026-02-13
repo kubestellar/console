223abc3852bc479187a3773cc2a0d467f7843ea3
xonas1101 <aarushsingh1305@gmail.com>
2026-02-13 00:12:59 +0000

    Added icon in resourcebadges
    Signed-off-by: xonas1101 <aarushsingh1305@gmail.com>

--- web/src/components/modals/sections/ResourceBadges.tsx
+++ web/src/components/modals/sections/ResourceBadges.tsx
@@ -29,6 +29,7 @@ import {
   BellRing,
   File,
   FolderTree,
+  Package,
 } from 'lucide-react'
 import type { ResourceKind, ResourceContext } from '../types/modal.types'
 import { ClusterBadge } from '../../ui/ClusterBadge'
@@ -61,6 +62,7 @@ const RESOURCE_ICONS: Record<ResourceKind, typeof Box> = {
   NetworkPolicy: Network,
   Event: Zap,
   HelmRelease: Ship,
+  BuildpackImage: Package,
   ArgoApplication: GitBranch,
   Operator: Settings,
   CRD: Puzzle,
