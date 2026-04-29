25cd22735672211c18316270fbdf9f17911ee1cd
Andy Anderson <clubanderson@gmail.com>
2026-04-29 16:16:04 +0000

    🐛 Fix TS7006: add explicit string type to chartName parameter
    Signed-off-by: Andy Anderson <clubanderson@gmail.com>

--- web/src/components/layout/mission-sidebar/MissionSidebar.tsx
+++ web/src/components/layout/mission-sidebar/MissionSidebar.tsx
@@ -1486,7 +1486,7 @@ export function MissionSidebar() {
           onClose={() => setShowBrowser(false)}
           onImport={handleImportMission}
           initialMission={deepLinkMission || undefined}
-          onUseInMissionControl={(chartName) => {
+          onUseInMissionControl={(chartName: string) => {
             setShowBrowser(false)
             setPendingKubaraChart(chartName)
             setShowMissionControl(true)
