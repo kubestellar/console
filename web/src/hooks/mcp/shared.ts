0a92037239070f47f0bc92cf51c9e94457c2fd22
clubanderson <clubanderson@users.noreply.github.com>
2026-05-03 00:08:13 +0000

    fix: remove unused appendWsAuthToken import
    Signed-off-by: clubanderson <clubanderson@users.noreply.github.com>

--- web/src/hooks/mcp/shared.ts
+++ web/src/hooks/mcp/shared.ts
@@ -7,7 +7,6 @@ import { kubectlProxy } from '../../lib/kubectlProxy'
 import { registerCacheReset, triggerAllRefetches } from '../../lib/modeTransition'
 import { resetFailuresForCluster, resetAllCacheFailures } from '../../lib/cache'
 import { clusterCacheRef, setClusterCacheRefClusters } from './clusterCacheRef'
-import { appendWsAuthToken } from '../../lib/utils/wsAuth'
 import { MS_PER_MINUTE } from '../../lib/constants/time'
 import {
   LOCAL_AGENT_HTTP_URL,
