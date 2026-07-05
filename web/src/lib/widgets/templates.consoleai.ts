81c9b996abf59f6f071c300e8ad8c7f4f49e03dc
Andy Anderson <andy@clubanderson.com>
2026-07-05 10:33:42 -0400

    [scanner] fix: remove unused card assignment in template generators
    Signed-off-by: Scanner <scanner@kubestellar.io>

--- web/src/lib/widgets/templates.consoleai.ts
+++ web/src/lib/widgets/templates.consoleai.ts
@@ -1,8 +1,9 @@
 import { parseBlock, wrapOpen, wrapClose, issueButton } from './templates.shared'
 import { WIDGET_CARDS, type WidgetCardDefinition } from './widgetRegistry'
 
-export function generateConsoleAICardRender(cardType: string, card: WidgetCardDefinition | null, _title?: string): string {
-  card = card || WIDGET_CARDS[cardType]
+export function generateConsoleAICardRender(cardType: string, _card?: WidgetCardDefinition | null, _title?: string): string {
+  const card = _card || WIDGET_CARDS[cardType]
+  void card
 
   switch (cardType) {
     case 'console_ai_offline_detection':
