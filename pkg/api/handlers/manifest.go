30bf9a099d30d5019ed4fcb8cb61872e3062ec84
Andy Anderson <andy@clubanderson.com>
2026-07-10 18:53:10 -0400

    fix: add missing sanitize import to card_proxy.go
    Signed-off-by: Club Anderson <club.anderson@gmail.com>
    Signed-off-by: GitHub Actions <actions@github.com>

--- pkg/api/handlers/manifest.go
+++ pkg/api/handlers/manifest.go
@@ -18,6 +18,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 
 	"github.com/kubestellar/console/pkg/client"
+	"github.com/kubestellar/console/pkg/sanitize"
 	"github.com/kubestellar/console/pkg/store"
 )
 
